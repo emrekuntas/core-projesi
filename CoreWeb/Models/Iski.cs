13f5bfcefe4d47dad2a788f96ee60d7320acc59d
emre küntaş <kuntasemre@gmail.com>
2019-03-19 12:22:24 +0300

    Add project files.

---
+++ CoreWeb/Models/Iski.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWeb.Models
+{
+    public class Iski
+    {
+        public int ilkSayac { get; set; }
+        public int sonSayac { get; set; }
+        public string meskenTipi { get; set; }
+    }
+}
